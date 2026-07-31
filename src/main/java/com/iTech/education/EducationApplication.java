b41b9c5235577917bfdfe10191fff0ee7a28edf7
Duy Tin <nduytin13112005@gmail.com>
2026-08-01 04:09:39 +0700

    chore: create initial project structure

---
+++ src/main/java/com/iTech/education/EducationApplication.java
@@ -0,0 +1,14 @@
+package com.iTech.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationApplication {
+
+	public static void main(String[] args) {
+		System.out.println();
+		SpringApplication.run(EducationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/iTech/education/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.iTech.education.service.impl;
+
+import com.iTech.education.service.IUserService;
+
+public class UserServiceImpl implements IUserService {
+}
