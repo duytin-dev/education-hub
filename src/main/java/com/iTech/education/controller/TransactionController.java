0f10fef877df1c7603f6dd82cda65e78a3d01ecd
Duy Tin <nduytin13112005@gmail.com>
2026-08-05 15:10:01 +0700

    "feat:create new transaction controller"

---
+++ src/main/java/com/iTech/education/controller/TransactionController.java
@@ -0,0 +1,11 @@
+package com.iTech.education.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@Validated
+public class TransactionController {
+}
